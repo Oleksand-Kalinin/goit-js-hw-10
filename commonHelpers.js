97a8ba6cb2033f37ef98da0c0f4dbf569b7183ee
Oleksand-Kalinin <157061100+Oleksand-Kalinin@users.noreply.github.com>
2024-05-27 19:59:09 +0000

    Deploying to gh-pages from @ Oleksand-Kalinin/goit-js-hw-10@5e248f9797cc78a2de23a8df18f18ad6c3a7da88 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const t=document.querySelector(".btn-go-back");t.addEventListener("click",()=>window.location.href="./index.html");
+//# sourceMappingURL=commonHelpers.js.map
---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const n=document.querySelector(".btn-go-back");n.addEventListener("click",()=>window.location.href="./index.html");
+//# sourceMappingURL=commonHelpers2.js.map
